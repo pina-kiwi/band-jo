e304b2432a7a144a2414a4ddf2b595ab940a3c08
Laura <lsbeatty777@gmail.com>
2025-12-01 15:40:54 -0800

    Updated Character Sprites

--- Assets/DeleteMe.cs
+++ Assets/DeleteMe.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class DeleteMe : MonoBehaviour
+public partial class DeleteMe : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
