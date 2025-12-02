631382c65dec7bdc737f5635bda1247bcb605755
laurenbautista <laurenbautista431@gmail.com>
2025-12-02 15:50:33 -0800

    animation

--- Assets/Scripts/SpriteTools.cs
+++ Assets/Scripts/SpriteTools.cs
@@ -4,7 +4,31 @@ using UnityEngine;
 
 public static class SpriteTools
 {
+    
     public static Vector3 ConstrainToScreen(SpriteRenderer spriteRenderer)
+    {
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(spriteRenderer.transform.position);
+
+        // if we're offscreen to the right
+        if (SpriteRight(spriteRenderer) > Screen.width)
+            screenPosition.x = Screen.width - SpriteHalfWidth(spriteRenderer);
+
+        // if we're offscreen to the left
+        if (SpriteLeft(spriteRenderer) < 0)
+            screenPosition.x = SpriteHalfWidth(spriteRenderer);
+
+        // if we're offscreen to the top
+        if (SpriteTop(spriteRenderer) > Screen.height)
+            screenPosition.y = Screen.height - SpriteHalfHeight(spriteRenderer);
+
+        // if we're offscreen to the bottom
+        if (SpriteBottom(spriteRenderer) < 0)
+            screenPosition.y = SpriteHalfHeight(spriteRenderer);
+
+        return Camera.main.ScreenToWorldPoint(screenPosition);
+    }
+    
+    public static Vector3 ConstrainToSquare(SpriteRenderer spriteRenderer)
     {
         Vector3 screenPosition = Camera.main.WorldToScreenPoint(spriteRenderer.transform.position);
 
