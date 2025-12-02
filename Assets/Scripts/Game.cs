6252001cef79a29cca2cd3d5f97de6494bd5dd4d
laurenbautista <laurenbautista431@gmail.com>
2025-12-02 12:07:08 -0800

    heart movement

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public Player Player;
-    public bool isPlaying = false;
+    public Move2D Move2D;
+    public KeyboardInput KeyboardInput;
+    public bool isPlaying = true;
     
     //game start
-    
+    public void PlayerDefenseStart()
+    {
+        if (isPlaying)
+        {
+            
+        }
+    }
     
     //player is able to move and dodge attacks
     
