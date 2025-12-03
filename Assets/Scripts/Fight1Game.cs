17a68b818211ca65c54faab5dfa483c3130a4169
laurenbautista <laurenbautista431@gmail.com>
2025-12-03 13:52:27 -0800

    GameFight script created

--- Assets/Scripts/Fight1Game.cs
+++ Assets/Scripts/Fight1Game.cs
@@ -1,20 +1,11 @@
 using UnityEngine;
 
-public class Game : MonoBehaviour
+public class Fight1Game : MonoBehaviour
 {
     public Player Player;
     public Move2D Move2D;
     public KeyboardInput KeyboardInput;
-    public bool isPlaying = true;
     
-    //game start
-    public void PlayerDefenseStart()
-    {
-        if (isPlaying)
-        {
-            
-        }
-    }
     
     //player is able to move and dodge attacks
     
